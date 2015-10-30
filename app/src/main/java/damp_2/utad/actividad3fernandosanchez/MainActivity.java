b00b324332bf5335d525ca70e1891f837b4fc5c4
fernando.sanchez <fsm_031@hotmail.com>
2015-10-30 11:04:16 +0100

    Se crea toda la segunda vista y se oculta (gone) para implementar la parte de botones de la primera.  Se crea una clase ButtonListener la cual va a controlar las acciones de los botones de nuestra aplicacion.  En la clase MainActivity se crean los botonos y editables que se usan. se referencian y se inicializan en el onCreate.

--- app/src/main/java/damp_2/utad/actividad3fernandosanchez/MainActivity.java
+++ app/src/main/java/damp_2/utad/actividad3fernandosanchez/MainActivity.java
@@ -3,12 +3,64 @@ package damp_2.utad.actividad3fernandosanchez;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+
+    ButtonListeners btnListener;
+
+    public EditText edtxtUsuario = null;
+    public EditText edtxtPass = null;
+
+
+    public LinearLayout activity_layout1=null;
+    public LinearLayout activity_layout2=null;
+
+    public Button btnLogin=null;
+    public Button btnRegistrarse=null;
+
+
+    public void init_buttons() {
+        btnListener=new ButtonListeners(this);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+
+        btnLogin.setOnClickListener(btnListener);
+        btnRegistrarse.setOnClickListener(btnListener);
+
+
+    }
+
+    public void init_text(){
+        edtxtUsuario=(EditText) findViewById(R.id.edtxtUsuario);
+        edtxtPass=(EditText) findViewById(R.id.edtxtPass);
+
+
+    }
+
+    public void initLayouts(){
+
+        activity_layout1=(LinearLayout)findViewById(R.id.activity_layout1);
+        activity_layout2=(LinearLayout)findViewById(R.id.activity_layout2);
+
+    }
+
+    public void cambiarPag() {
+        activity_layout1.setVisibility(View.GONE);
+        activity_layout2.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.initLayouts();
+        this.init_buttons();
+        this.init_text();
     }
 }
