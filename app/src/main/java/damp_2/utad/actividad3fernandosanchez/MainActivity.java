d09444036de04ffd659585df3b316d2ae65d2229
fernando.sanchez <fsm_031@hotmail.com>
2015-10-30 09:53:46 +0100

    Initial revision

---
+++ app/src/main/java/damp_2/utad/actividad3fernandosanchez/MainActivity.java
@@ -0,0 +1,14 @@
+package damp_2.utad.actividad3fernandosanchez;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
